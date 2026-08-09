08a3e164f75b7d7a601640999c86afc1ecad6ceb
Pragana Shrestha <shresthapragana@gmail.com>
2026-08-09 13:11:35 +0545

    Set up server structure and MySQL database connection

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.get('/', (req, res) => {
+  res.send('Illam Chiya API is live!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+db.getConnection()
+  .then((connection) => {
+    console.log('Database connected successfully!');
+    connection.release();
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  })
+  .catch((err) => {
+    console.log('DB connection issue:', err.message);
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  });
