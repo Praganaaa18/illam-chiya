66041a609eaa3d3fb05e25fe571565257b6e84dd
Khushi Singh <khushi@gmail.com>
2026-08-17 15:12:38 +0545

    Build Buyer Registration page component and route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import RegisterBuyer from './components/RegisterBuyer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register-buyer" element={<RegisterBuyer />} />
       </Routes>
     </Router>
   );
