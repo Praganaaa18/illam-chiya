b6e926cf5959a91a692de37532a44913000e2d8b
Khushi Singh <khushi@gmail.com>
2026-08-19 11:42:19 +0545

    Add seller registration page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import all your components
 import Home from './components/Home';
 import Login from './components/Login';
 import RegisterBuyer from './components/RegisterBuyer';
+import RegisterSeller from './components/RegisterSeller';
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register-buyer" element={<RegisterBuyer />} />
+        <Route path="/register-seller" element={<RegisterSeller />} />
       </Routes>
     </Router>
   );
